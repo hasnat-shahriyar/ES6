71f21682c597b38b8c1ebd22c2a9253123d08dc5
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-19 19:21:48 +0600

    destructuring using function

--- ES-L1/destructuring.js
+++ ES-L1/destructuring.js
@@ -1,8 +1,18 @@
 // Object and Array Destructuring
+
+// Object
 const bio = {Name : "Hasnat", Age : 23, Profession : "Student"}
 const {Name, Age, Profession} = bio;
 console.log (Name);
 
+// Array
 const friends = ["Nahin", "Emon", "Masud", "Shad"]
 const [me, firstFriend, secondFriend, thirdFriend] = friends;
 console.log (me, firstFriend, thirdFriend);
+
+// Function
+function getNames (){
+    return ["Eti", "Baki"];
+}
+const [ammu, abbu] = getNames();
+console.log (ammu, abbu);
