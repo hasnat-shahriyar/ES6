52778be7a0287a531f5b179b72d6fc640bf15cbe
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-19 15:32:21 +0600

    default parameter

--- ES-L1/default.js
+++ ES-L1/default.js
@@ -5,3 +5,11 @@ function add (first, second = 10){
 
 const result = add (10);
 console.log (result);
+
+function fullName (firstName, lastName = "Shahriyar"){
+    const name = firstName + " " + lastName;
+    return name;
+}
+
+const name = fullName("Hasnat");
+console.log (name);
