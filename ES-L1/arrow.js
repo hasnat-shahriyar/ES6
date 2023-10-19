ba43697aeb58d7ac6b1a6cd5242d3fc01eb1303c
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-19 16:11:26 +0600

    arrow function, multiple parameter, function body

---
+++ ES-L1/arrow.js
@@ -0,0 +1,13 @@
+// Arrow function, multiple parameter, function body
+const add = function(first, second){
+    return first + second;
+}
+// Arrow Function
+const add2 = (first, second) => first + second;
+
+const result = add(5, 10);
+const result2 = add2(10, 20);
+console.log (result);
+console.log (result2);
+
+// Interview Question : Differences between Function Declaration, Function Expression and Arrow Function
