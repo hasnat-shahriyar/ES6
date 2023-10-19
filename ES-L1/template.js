a8c7aef4c34c79a3070c529a4260c4552c521d28
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-19 15:42:00 +0600

    template multiline method

---
+++ ES-L1/template.js
@@ -0,0 +1,15 @@
+const person = "Hasnat Shahriyar";
+const person2 = 'Shad Hossain';
+const person3 = `Tafsinur Rahman`;
+
+const multiLine = "First Line\n"+
+"Second Line\n" +
+"Third Line\n";
+
+const newMultiLineMethod =`
+First Line Method
+Second Line Method
+Third Line Method
+`
+
+console.log(multiLine, newMultiLineMethod);
