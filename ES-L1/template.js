763ba5a023d4c3b0c2a303b65a370dc320382f5a
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-19 15:51:18 +0600

    template string, multiple line string, dynamic string

--- ES-L1/template.js
+++ ES-L1/template.js
@@ -1,3 +1,4 @@
+// Template string, multiple line string, dynamic string
 const person = "Hasnat Shahriyar";
 const person2 = 'Shad Hossain';
 const person3 = `Tafsinur Rahman`;
@@ -13,3 +14,12 @@ Third Line Method
 `
 
 console.log(multiLine, newMultiLineMethod);
+
+const a = 20;
+const b = 30;
+const nums = [10, 20, 30, 40, 50]
+const summary = "Sum of : " + a + " and" + b + " is : " + (a+b);
+// Template String
+const newSummary = `\nSum of ${a} and ${nums.length} is : ${a+b}`;
+
+console.log (summary, newSummary);
