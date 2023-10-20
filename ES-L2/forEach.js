285bb04e986a2e032f32a8126f5db7e605263e12
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-20 12:19:38 +0600

    forEach

---
+++ ES-L2/forEach.js
@@ -0,0 +1,10 @@
+const friends = ["Rakib", "Mehedi", "Noyon", "Mahir"];
+
+friends.forEach( friend => console.log(friend));
+
+const products = [
+    {id: 1, name: "laptop", price: 65000},
+    {id: 2, name: "mobile", price: 15000},
+    {id: 3, name: "watch", price: 5000},
+    {id: 4, name: "earphone", price: 1500},
+];
