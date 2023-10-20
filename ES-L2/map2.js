c29859242ceb5870a3b68db2e52d3a5586e955d8
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-20 12:15:29 +0600

    map practice 2

--- ES-L2/map2.js
+++ ES-L2/map2.js
@@ -8,3 +8,14 @@ console.log (firstLetters);
 
 const nameLength = friends.map (friends => friends.length);
 console.log (nameLength);
+
+const products = [
+    {id: 1, name: "laptop", price: 65000},
+    {id: 2, name: "mobile", price: 15000},
+    {id: 3, name: "watch", price: 5000},
+    {id: 4, name: "earphone", price: 1500},
+];
+
+const items = products.map (product => console.log(product.name));
+const prices = products.map (p => p.price);
+console.log(prices);
