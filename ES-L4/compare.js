2e405ed5d7e2d47fc53870a06037c9b21e33290c
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-28 01:54:57 +0600

    comparing two primitive values

---
+++ ES-L4/compare.js
@@ -0,0 +1,16 @@
+// Comparing two primitive values
+const a = 2;
+const b = 2;
+
+if (a === b) {
+    console.log("They are the same");
+} else {
+    console.log("They are different");
+}
+
+// Output: They are the same
+
+// In this case, the values of 'a' and 'b' are both 2, which is a primitive type. 
+// When you compare primitive values like numbers using '===', it checks if their values are identical, so they are considered the same.
+
+
