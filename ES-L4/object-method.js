09e02c491ce652c0ef22ed461804b9b363764e71
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-28 00:52:58 +0600

    adding good comments

--- ES-L4/object-method.js
+++ ES-L4/object-method.js
@@ -1,20 +1,27 @@
 // Object method access and set property values
+// Define an object called 'aboutMe'
 const aboutMe = {
-    Name : "Hasnat Shahiyar",
-    Salary : 25000,
-    Designation : "Front End Developer",
-    Skills : ["HTML", "CSS", "JavaScript"],
-    Experience : function (){
-        return this.Name + " has a experience of 3 months of internship";
+    Name: "Hasnat Shahiyar",
+    Salary: 25000,
+    Designation: "Front End Developer",
+    Skills: ["HTML", "CSS", "JavaScript"],
+    
+    // Method to calculate and return the experience
+    Experience: function () {
+        return this.Name + " has an experience of 3 months of internship";
     },
-    newSkill : function (skill){
-        this.Experience ();
-        return `${this.Name} is enhancing and improving his skills on ${skill}`
+    
+    // Method to add a new skill and return a message
+    newSkill: function (skill) {
+        this.Experience(); // Call the 'Experience' method to get the experience message.
+        return `${this.Name} is enhancing and improving his skills in ${skill}`;
     }
 }
 
+// Call the 'Experience' method and store the result in 'finalOutput'
 const finalOutput = aboutMe.Experience();
-console.log (finalOutput);
+console.log(finalOutput); // Print the 'finalOutput' message to the console.
 
-const finalSkill = aboutMe.newSkill("React.js")
-console.log (finalSkill);
+// Call the 'newSkill' method with the argument "React.js" and store the result in 'finalSkill'
+const finalSkill = aboutMe.newSkill("React.js");
+console.log(finalSkill); // Print the 'finalSkill' message to the console.
