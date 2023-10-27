06eecb7dd260bd40593d0521dd214f92b2d8402c
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-28 00:59:58 +0600

    js object keys and values

---
+++ ES-L4/freez.js
@@ -0,0 +1,14 @@
+// Keys, values, entries, delete, seal, freeze
+
+const bottle = {
+    Product : "Water",
+    Price : 200,
+    Quantity : "500 ml",
+    Mineral : true,
+}
+// Keys
+const keys = Object.keys (bottle);
+console.log (keys);
+// Values
+const values = Object.values(bottle);
+console.log (values);
