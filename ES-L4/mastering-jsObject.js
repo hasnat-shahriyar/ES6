f93e8e464a0c7c8a5202b9a9ca4d68f87807d73c
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-27 14:25:05 +0600

    using object create method

--- ES-L4/mastering-jsObject.js
+++ ES-L4/mastering-jsObject.js
@@ -11,4 +11,7 @@ bio.skills = function(){
 
 console.log(bio); // Print the 'bio' object and its properties to the console.
 
+// Object Create Method
+const nahin = Object.create(bio); // Create a new object 'nahin' based on the 'bio' object.
+console.log(nahin.age); // Access and print the 'age' property from the 'nahin' object, which is inherited from 'bio'.
 
