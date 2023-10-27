d611d2237ae1e676c0c41c283da208a6889c6026
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-28 01:11:28 +0600

    adding good comment

--- ES-L4/freez.js
+++ ES-L4/freez.js
@@ -1,24 +1,45 @@
-// Keys, values, entries, delete, seal, freeze
-
+// Define an object called 'bottle'
 const bottle = {
-    Product : "Water",
-    Price : 200,
-    Quantity : "500 ml",
-    Mineral : true,
-}
-// Keys
-const keys = Object.keys (bottle);
-console.log (keys);
-// Values
+    Product: "Water",
+    Price: 200,
+    Weight: "500 ml",
+    Mineral: true,
+};
+
+// Object.keys() method - Get an array of property keys
+const keys = Object.keys(bottle);
+console.log(keys); // Output: ["Product", "Price", "Weight", "Mineral"]
+
+// Object.values() method - Get an array of property values
 const values = Object.values(bottle);
-console.log (values);
-// Entries
-const pair = Object.entries (bottle);
-console.log (pair);
-// Delete
+console.log(values); // Output: ["Water", 200, "500 ml", true]
+
+// Object.entries() method - Get an array of key-value pairs (entries)
+const pair = Object.entries(bottle);
+console.log(pair);
+// Output: 
+// [
+//    ["Product", "Water"],
+//    ["Price", 200],
+//    ["Weight", "500 ml"],
+//    ["Mineral", true]
+// ]
+
+// Delete the 'Mineral' property from the 'bottle' object
 delete bottle.Mineral;
 console.log(bottle);
-// Seal
+// Output: { Product: "Water", Price: 200, Weight: "500 ml" }
+
+// Object.seal() method - Seal the 'bottle' object to prevent adding or deleting properties
 Object.seal(bottle);
-delete bottle.Quantity;
+delete bottle.Weight; // Deleting a property (doesn't work)
+bottle.Quantity = 1; // Adding a property (doesn't work)
 console.log(bottle);
+// Output: { Product: "Water", Price: 200, Weight: "500 ml" }
+
+// Object.freeze() method - Freeze the 'bottle' object to make it completely immutable
+Object.freeze(bottle);
+// Attempt to modify the 'Price' property (doesn't work, no change)
+bottle.Price = 250;
+console.log(bottle);
+// Output: { Product: "Water", Price: 200, Weight: "500 ml" }
