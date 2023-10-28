5993fea3ba02314de306cfa5121f2187e134a842
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-28 13:08:47 +0600

    true and false in javascript

---
+++ ES-L5/true-false.js
@@ -0,0 +1,28 @@
+const x = 5;
+const y = 0;
+const z = "String is truth"
+const a = ""
+
+if (x) {
+    console.log ("The value of x is Truthy");
+} else {
+    console.log ("The value of x is Falsy");
+}
+
+if (y) {
+    console.log ("The value of x is Truthy");
+} else {
+    console.log ("The value of x is Falsy");
+}
+
+if (z) {
+    console.log ("The value of x is Truthy");
+} else {
+    console.log ("The value of x is Falsy");
+}
+
+if (a) {
+    console.log ("The value of x is Truthy");
+} else {
+    console.log ("The value of x is Falsy");
+}
