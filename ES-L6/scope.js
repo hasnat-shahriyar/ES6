5a0e2fdeb8fae1d75910fdd2045eebe1d56a0696
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-29 16:02:15 +0600

    scope in javascript

---
+++ ES-L6/scope.js
@@ -0,0 +1,20 @@
+// Block scope global scope simple understanding of Hoisting
+
+function add(a, b) {
+    // Inside the function, we create a new variable 'total' and set it to the result of multiplying 'a' and 'b'.
+    const total = a * b;
+
+    // We then log the values of 'a' and 'b' to the console, so you can see what values were passed to the function.
+    console.log(a, b);
+
+    // Finally, we return the value of 'total' as the result of the function.
+    return total;
+}
+
+// If I uncomment them, I'll get errors because 'a', 'b', and 'total' are not defined outside the 'add' function.
+// console.log(a, b); // This will result in an error because 'a' and 'b' are not defined here.
+// console.log(total); // This will also result in an error because 'total' is not defined here.
+// I cannot use them outside the function if I do use them then it will show an error
+
+// This line calls the 'add' function with the values 5 and 7 as its arguments and displays the result.
+add(5, 7);
