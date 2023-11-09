b551601ba24a8a28a251fd5cd1d699f7e72ce854
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-11-10 01:30:37 +0600

    learning async

---
+++ ES-L7/async.js
@@ -0,0 +1,18 @@
+console.log (1);
+console.log (2);
+doSomething ();
+console.log (4);
+empty()
+setTimeout (six);
+console.log (6);
+
+function empty (){
+    console.log ()
+}
+
+function doSomething (){
+    console.log (3);
+}
+function six (){
+    console.log (5);
+}
