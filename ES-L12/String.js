c3fa3d78d39888775b649f1c782503a89faaaf87
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-11 21:46:51 +0600

    string

---
+++ ES-L12/String.js
@@ -0,0 +1,14 @@
+const named = 'Hasnat Shahriyar Nahin';
+const start = named.startsWith('Hasnat');
+const End = named.endsWith('Nahin');
+const include = named.includes('Shahriyar');
+
+console.log(start);
+console.log(End);
+console.log(include);
+
+const rpt = "Alhamdulillah ";
+const repeatedRpt = rpt.repeat(5);
+console.log(repeatedRpt);
+
+console.log(`I always say - ${'Alhamdulillah '.repeat(5)}`);
