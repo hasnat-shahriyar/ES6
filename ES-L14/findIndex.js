55e202d56a5dc8d81208e61f2d87f08b7d899f4f
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-12 21:24:17 +0600

    for loop in es6 and findIndex

---
+++ ES-L14/findIndex.js
@@ -0,0 +1,4 @@
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 90, 10];
+
+let result = numbers.findIndex(num => num === 90);
+console.log(result);
---
+++ ES-L14/for_loop_in_es6.js
@@ -0,0 +1,7 @@
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (num of numbers)
+{
+    if(num === 5) break;
+    console.log(`Number printed: ${num}`);
+}
