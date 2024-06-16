a0c3b7eaec97196c16252f83783d3983803f9bf1
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-16 21:02:53 +0600

    Math in js

---
+++ ES-L21/Math.js
@@ -0,0 +1,14 @@
+console.log(Math.PI);
+console.log(Math.pow(4, 2));
+console.log(Math.floor(5.515));
+console.log(Math.max(1, 20, 10, 30, 40, 50, 60, 90, 70, 80, 100));
+console.log(Math.min(1, 20, 10, 30, 40, 50, 60, 90, 70, 80, 100));
+
+let numbers = [10, 20,30,40,50,60,70,80,90];
+console.log(Math.max(...numbers));
+console.log(Math.min(...numbers));
+
+console.log(Math.floor(Math.random() * 100));
+
+console.log(Math.round(10.49));
+console.log(Math.round(10.50));
