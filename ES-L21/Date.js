908c0edc7bfb29e3b37e0b52cfeddda0a29c9e38
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-06-16 21:40:18 +0600

    Date object

---
+++ ES-L21/Date.js
@@ -0,0 +1,8 @@
+console.log(Date());
+
+const date = Date();
+console.log(typeof(date));
+
+const date_2 = new Date();
+console.log(date_2.getDate(), date_2.getMonth(), date_2.getFullYear());
+console.log(typeof(date_2));
